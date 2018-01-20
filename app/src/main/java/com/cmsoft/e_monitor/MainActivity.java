7579b23a8935ff10ca531f22f1bad6f7a8758f73
Cesar Matute <cesarmatute@gmail.com>
2018-01-20 01:17:00 -0400

    Los terremotos se cargan de la base de datos si no hay conexion a internet

--- app/src/main/java/com/cmsoft/e_monitor/MainActivity.java
+++ app/src/main/java/com/cmsoft/e_monitor/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cmsoft.e_monitor;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -37,22 +39,52 @@ public class MainActivity extends AppCompatActivity implements DownloadEqsAsyncT
         earthquakeListView = (ListView) findViewById(R.id.earthquake_listview);
 
         if (Utils.isnetworkAvailable(this)) {
-            DownloadEqsAsyncTAsk downloadEqsAsyncTAsk = new DownloadEqsAsyncTAsk(this);
-            downloadEqsAsyncTAsk.delegate = this;
-            try {
-                downloadEqsAsyncTAsk.execute(new URL("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"));
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
+            downloadEarthquakes();
         } else {
-            //Leer la base de datos
+            getEarthquakesFromDb();
         }
 
 
     }
 
+    private void getEarthquakesFromDb() {
+        EqDbHelper dbHelper = new EqDbHelper(this);
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
+        Cursor cursor = database.query(EqContract.EqColumns.TABLE_NAME,null,null,null,null,null,null);
+
+        ArrayList<Earthquake> eqList = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            Long dateTime = cursor.getLong(EqContract.EqColumns.TIMESTAMP_COLUMN_INDEX);
+            Double magnitude = cursor.getDouble(EqContract.EqColumns.MAGNITUDE_COLUMN_INDEX);
+            String place = cursor.getString(EqContract.EqColumns.PLACE_COLUMN_INDEX);
+            String longitude = cursor.getString(EqContract.EqColumns.LONGITUDE_COLUMN_INDEX);
+            String latitude = cursor.getString(EqContract.EqColumns.LATITUDE_COLUMN_INDEX);
+
+            eqList.add(new Earthquake(dateTime, magnitude, place, longitude, latitude));
+        }
+
+        cursor.close();
+        fillEqList(eqList);
+    }
+
+    private void downloadEarthquakes() {
+        DownloadEqsAsyncTAsk downloadEqsAsyncTAsk = new DownloadEqsAsyncTAsk(this);
+        downloadEqsAsyncTAsk.delegate = this;
+        try {
+            downloadEqsAsyncTAsk.execute(new URL("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onEqsDownloaded(ArrayList<Earthquake>eqList) {
+        fillEqList(eqList);
+    }
+
+    private void fillEqList(ArrayList<Earthquake>eqList) {
         final EqAdapter eqAdapter = new EqAdapter(this, R.layout.eq_list_item, eqList);
         earthquakeListView.setAdapter(eqAdapter);
 
